3b1928125afa0cdcdc52ef978141899cfd3fd9df
MartinDrost <info@martindrost.nl>
2016-03-09 11:48:38 +0100

    Added huffman

---
+++ Huffman/src/sample/Controller.java
@@ -0,0 +1,8 @@
+package sample;
+
+public class Controller {
+    public  String text = "Lollipop jelly beans jelly apple pie. Fruitcake biscuit caramels lollipop pudding croissant. " +
+            "Gingerbread tiramisu wafer cake jelly beans dessert jelly beans chocolate cake. Powder ice cream apple pie " +
+            "sesame snaps gummi bears sweet caramels croissant. Wafer jelly carrot cake pie powder caramels soufflé " +
+            "icing bonbon. Powder gummies carrot cake bonbon cheesecake tiramisu macaroon.";
+}
